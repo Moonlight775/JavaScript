7f1ae46b24224bd7de31c02b562525391c842fee
KwanYoung Moon <pianoguys775@gmail.com>
2021-11-05 00:20:49 +0900

    variable

--- src/main/webapp/javascript/variable.js
+++ src/main/webapp/javascript/variable.js
@@ -10,3 +10,22 @@
 	console.log(name);
 	name = 'hello';
 	console.log(name);
+
+	// block scope
+	// {}밖에서는 {}안의 내용을 볼 수 없다.
+	// {}밖에서 console.log로 불러보려해도 불러지지않는다.
+	{
+	let name = 'ellie';
+	console.log(name);
+	name = 'hello';
+	console.log(name);
+	}
+	console.log(name);
+	
+	// global scope
+	// 어디서나 접근 가능 
+	// 어플리케이션이 실행되는 순간부터 끝날때까지 메모리에 탑재되어 있기 때문에 최소한으로 사용
+	let globalName = 'global name';
+	console.log(globalName);
+	
+	
