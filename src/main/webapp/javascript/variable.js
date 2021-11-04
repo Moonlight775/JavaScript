4121c790313f679f41f29047794197aed960aa93
KwanYoung Moon <pianoguys775@gmail.com>
2021-11-05 00:06:39 +0900

    initial commit and start javascript

---
+++ src/main/webapp/javascript/variable.js
@@ -0,0 +1,12 @@
+	// 1. Use strict
+	// added in ES 5
+	// use this for Vanilla Javascript
+	'use strict';
+	
+	// 2. Variable
+	// let (added in Es6)
+	
+	let name = 'ellie';
+	console.log(name);
+	name = 'hello';
+	console.log(name);
