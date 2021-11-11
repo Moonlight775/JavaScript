dd311bbf7cfae7d9b7becd7c606ef8b66f733a5e
KwanYoung Moon <pianoguys775@gmail.com>
2021-11-11 23:16:45 +0900

    Study 'bout class in JavaScript

--- src/main/webapp/javascript/class.js
+++ src/main/webapp/javascript/class.js
@@ -49,10 +49,107 @@
 		//	if (value < 0) {
 		//		throw Error('age can not be negative');
 		//	}
-			this._age = value < 0 ? 0 : value;
+			this._age = value < 0 ? 0 : value ;
 		}
 	}
 	
 	const user1 = new User('Steve', 'Job', -1);
 	console.log(user1.age);
+	
+	// 3. Fields (public, private)
+	// Too soon! 참고만할 것
+	class Experiment {
+		publicField = 2;
+		#privateField = 0;
+	};
+	const experiment = new Experiment();
+	console.log(experiment.publicField);
+	console.log(experiment.privateField);
+	
+	// 4. Static properties and methods
+	// Too soon! 마찬가지로 참고만 할 것
+	class Article {
+		static publisher = 'Dream Coding';
+		constructor(articleNumber) {
+			this.articleNumber = articleNumber;
+		}
+		
+		static printPublisher() {
+			console.log(Article.publisher);
+		}
+	}
+	
+	const article1 = new Article(1);
+	const article2 = new Article(2);
+	
+	// object안에 publisher는 값이 지정되지 않음
+	// static은 object 마다 할당되는 것이 아니라 class 자체에 붙어 있음
+	console.log(article1.publisher); // undefined
+	console.log(Article.publisher); // Dream Coding
+	Article.printPublisher(); // Dream Coding
+	
+	// 5. Inheritance
+	// a way for one class to extend another class.
+	class Shape {
+		constructor(width, height, color) {
+			this.width = width;
+			this.height = height;
+			this.color = color;
+		}
+		
+		draw() {
+			console.log(`drawing ${this.color} color of`);
+		}
+		
+		getArea() {
+			return this.width * this.height;
+		}
+	}
+	
+	class Rectangle extends Shape {}
+	class Triangle extends Shape {
+		//overridng
+		draw() {
+			console.log('▲');
+		}
+		
+		getArea() {
+			return (this.width * this.height) / 2;
+		}
+	}
+	
+	const rectangle = new Rectangle(20, 20, 'blue');
+	rectangle.draw();
+	console.log(rectangle.getArea());
+	const triangle = new Triangle(20, 20, 'red');
+	triangle.draw();
+	console.log(triangle.getArea());
+	 
+	// 6. Class checking: instancdOf
+	console.log(rectangle instanceof Rectangle);
+	console.log(triangle instanceof Rectangle);
+	console.log(triangle instanceof Triangle);
+	console.log(triangle instanceof Shape);
+	console.log(triangle instanceof Object);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
--- src/main/webapp/javascript/function.js
+++ src/main/webapp/javascript/function.js
@@ -157,6 +157,24 @@
 	// function calculate(command, a, b)
 	// command: add, substract, divide, multiply, remainder
 	
+	function calculate(command, a, b) {
+		switch (command) {
+			case 'add':
+				return a + b;
+			case 'substract':
+				return a - b;
+			case 'divide':
+				return a / b;
+			case 'multiply':
+				return a * b;
+			case 'remainder':
+				return a % b;
+			default:
+				throw Error('unkown command');
+		}
+	}
+	console.log(calculate('add', 2, 3));
+	
 	
 	
 	
