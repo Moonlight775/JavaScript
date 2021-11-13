2679a473b5c10bbd8a3f0fca336a6bfe04bd0b10
KwanYoung Moon <pianoguys775@gmail.com>
2021-11-13 23:41:24 +0900

    for..in, for..of & cloning

--- src/main/webapp/javascript/object.js
+++ src/main/webapp/javascript/object.js
@@ -79,14 +79,44 @@
 	// 6. for..in vs for..of
 	// for (kye in obj)
 	console.clear();
-	for (key in ellie) {
+	for (key in moon) {
 		console.log(key);
 	}
 	
+	// for (value of iterable)
+	const array = [1, 2, 4, 5];
+	// for(let i = 0; i < array.length ; i++) {
+	//	console.log(array[i]);
+	// }
+	for(value of array) {
+		console.log(value);
+	}
 	
+	// 7. cloning
+	// Object.assign(dest, [obj1, obj2, obj3...])
+	const user = { name: 'moon', age: '20' };
+	const user2 = user;	
+	user2.name = 'coder';
+	console.log(user);
 	
+	// old way
+	const user3 = {};
+	for (key in user) {
+		user3[key] = user[key];
+	}
+	console.log(user3);
 	
+	const user4 = {};
+	Object.assign(user4, user);
+	// const user4 = Object.assign({}, user);
+	console.log(user4);
 	
+	// another example
+	const fruit1 = { color: 'red' };
+	const fruit2 = { color: 'blue', size: 'big' };
+	const mixed = Object.assign({}, fruit1, fruit2);
+	console.log(mixed.color);
+	console.log(mixed.size);
 	
 	
 	
