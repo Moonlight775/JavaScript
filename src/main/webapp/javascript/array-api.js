1445924cc7bd82dc46077c32a84305f3fe0d08d3
KwanYoung Moon <pianoguys775@gmail.com>
2021-11-15 23:45:36 +0900

    array quiz

---
+++ src/main/webapp/javascript/array-api.js
@@ -0,0 +1,9 @@
+	'use strict';
+	
+	// Q1. make a string out of an array
+	{
+		const fruits = ['apple', 'banana', 'orange'];
+		console.log(fruits.toString());
+		const fruitsString = fruits.join(', what ');
+		console.log(fruitsString);
+	}
