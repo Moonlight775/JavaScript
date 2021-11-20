47d3558ea5dd0faa111104f9357a48b991e3f58e
KwanYoung Moon <pianoguys775@gmail.com>
2021-11-20 21:23:43 +0900

    JSON to Object -> parse(json)

--- src/main/webapp/javascript/json.js
+++ src/main/webapp/javascript/json.js
@@ -34,3 +34,40 @@
 	
 	// 2. JSON to Object
 	// parse(json)
+	console.clear();
+	json = JSON.stringify(rabbit);
+	console.log(json);
+	const obj = JSON.parse(json);
+	const obj1 = JSON.parse(json, (key, value) => {
+		console.log(`key: ${key}, value: ${value}`);
+		return key === 'birthDate' ? new Date(value) : value;
+	});
+	console.log(obj);
+	console.log(obj1);
+	rabbit.jump();
+	// obj.jump(); // rabbit을 JSON으로 변환 시 method는 미포함, 다시 object로 변환 시 데이터만 들어감
+	
+	console.log(rabbit.birthDate.getDate());
+	//console.log(obj.birthDate.getDate()); // json 변환 시 Date()가 String이 되면서 Uncaught TypeError 발생
+	console.log(obj1.birthDate.getDate());
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
