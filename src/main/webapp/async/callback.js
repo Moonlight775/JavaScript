43edd8577311c29b1ce4d07222a56104250f705f
KwanYoung Moon <pianoguys775@gmail.com>
2021-11-26 00:45:24 +0900

    Synchronous callback & Asynchronous callback

--- src/main/webapp/async/callback.js
+++ src/main/webapp/async/callback.js
@@ -5,7 +5,19 @@
 	// hoisting: var, function declaration 선언들이 자동적으로 제일 위로 올라감
 	
 	console.log('1');
-	setTimeout(function() {
+	setTimeout(function() { // setTimeout(() => console.log('2'), 1000);
 		console.log('2');
 	}, 1000);
 	console.log('3');
+	
+	// Synchronous callback
+	function printImmediately(print) {
+		print();
+	}
+	printImmediately(() => console.log('hello'));
+	
+	// Asynchronous callback
+	function printWithDelay(print, timeout) {
+		setTimeout(print, timeout);
+	}
+	printWithDelay(()=> console.log('async callback'), 2000);
