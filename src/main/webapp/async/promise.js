25ddaaaaf5967542a34d2531778dffe133b243a7
KwanYoung Moon <pianoguys775@gmail.com>
2021-11-28 19:01:42 +0900

    Study 'bout promise.then(), .catch(), .finally()

--- src/main/webapp/async/promise.js
+++ src/main/webapp/async/promise.js
@@ -11,7 +11,17 @@
 		console.log('doing something...');
 		setTimeout(() => {
 			resolve('moon');
+			//reject(new Error('no network'));
 		}, 2000);
 	});
 	
-	
+	// 2. Consumers: then, catch, finally
+	promise.then(value => {
+		console.log(value);
+	})
+	.catch(error => {
+		console.log(error);
+	})
+	.finally(() => {
+		console.log('finally');
+	});
