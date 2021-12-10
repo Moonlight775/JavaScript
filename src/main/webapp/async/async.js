bbd0a5ffaa4cb5b4db8eaee82c47d194c3875a92
KwanYoung Moon <pianoguys775@gmail.com>
2021-12-10 21:30:18 +0900

    async - await

--- src/main/webapp/async/async.js
+++ src/main/webapp/async/async.js
@@ -22,3 +22,51 @@
 
     const user2 = fetchUser2();
     user2.then(console.log);
+
+    // 2. await
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function getApple() {
+        await delay(1000);
+        return 'apple';
+    }
+
+    async function getBanana() {
+        await delay(1000);
+        return 'banana';
+    }
+    // function getBanana() {
+    //     return delay(3000)
+    //         .then(() => 'banana');
+    // }
+
+    // function pickFruits() {
+    //     return getApple()
+    //         .then(apple => {
+    //             return getBanana()
+    //                 .then(banana => `${apple} + ${banana}`);
+    //         });
+    // }
+
+    async function pickFruits() {
+        const apple= await getApple();
+        const banana = await getBanana();
+        return `${apple} + ${banana}`;
+    }
+
+    pickFruits().then(console.log);
+
+
+
+
+
+
+
+
+
+
+
+
+
